3e1e3f1c5033235d480ae11362828e629376d5d9
Nimavat Mansi <144536348+NimavatMansi-19@users.noreply.github.com>
2024-12-16 11:18:14 +0530

    Add files via upload

---
+++ Asp.net/Lab 5/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////1. Create an ArrayList for StudentName and perform following operations:
+            ////a. Add() - To Add new student in list
+            ////b. Remove() - To Remove Student with specified index
+            ////c. RemoveRange() - To Remove student with specified range.
+            ////d. Clear()
+            ////- To clear all the student from the list 
+            //ArrayList StudentName = new ArrayList();
+            //StudentName.Add("Mansi");
+            //StudentName.Add("Darshi");
+            //StudentName.Add("Shruti");
+            //StudentName.Add("Riya");
+            //StudentName.Add("Nandani");
+            //StudentName.Remove("Mansi");
+            ////StudentName.RemoveAt(4);
+            //StudentName.RemoveRange(0, 2);
+            //StudentName.Clear();
+            //foreach (string i in StudentName)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //// 2.Create a List for StudentName and perform following operations:
+            //// a.Add() - To Add new student in list
+            //// b.Remove() - To Remove Student with specified index
+            //// c.RemoveRange() - To Remove student with specified range.
+            //// d.Clear() - To clear all the student from the list
+            //List<string> StudentName = new List<string>();
+            // StudentName.Add("Mansi");
+            // StudentName.Add("Darshi");
+            // StudentName.Add("Shruti");
+            // StudentName.Add("Riya");
+            // StudentName.Add("Nandani");
+
+            // StudentName.Remove("Riya");
+            // StudentName.RemoveRange(0, 3);
+            // StudentName.Clear();
+            // foreach (string i in StudentName)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // //3. Create a Stack which takes integer values and perform following operations:
+            // //a. Push() - To Add new item in stack
+            // //b. Pop() - To Remove item from the stack
+            // //c. Peek() – To Return the top item from the stack.
+            // //d. Contains() - To Checks whether an item exists in the stack or not.
+            // //e. Clear() - To clear items from stack 
+
+            // Stack s=new Stack();
+            // s.Push(1);
+            // s.Push(2);
+            // s.Push(3);
+            // s.Push(4);
+            // s.Push(5);
+            // Console.WriteLine("Poped Element is :"+s.Pop());
+            //Console.WriteLine("Peeked Element is "+s.Peek());//does not delete only return top element
+            // Console.WriteLine(s.Contains(3));
+            // s.Clear();
+            // foreach(int i in s)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // //4.Create a Queue which takes integer values and perform following operations:
+            // //    a.Enqueue() - Adds an item into the queue.
+            // //    b.Dequeue() - Returns an item from the beginning of the queue and removes it from the queue.
+            // //    c.Peek() - Returns an first item from the queue without removing it.
+            // //    d.Contains() - Checks whether an item is in the queue or not
+            // //    e.Clear() - Removes all the items from the queue
+
+            // Queue q=new Queue();
+            // q.Enqueue(1);
+            // q.Enqueue(2);
+            // q.Enqueue(3);
+            // q.Enqueue(4);
+            // q.Enqueue(5);
+            //Console.WriteLine("Removed element is: "+ q.Dequeue());
+            // Console.WriteLine("Top most element is : " + q.Peek());
+            // q.Clear();
+            //  foreach (int i in q)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            //// Create a Dictionary collection class object and preform following operations:
+            //// a.Add: Adds a key-value pair.
+            //// b.Remove: Removes a key-value pair by key.
+            //// c.ContainsKey: Checks if a key exists in the hashtable.
+            //// d.ContainsValue: Checks if a value exists in the hashtable.
+            //// e.Clear: Removes all key-value pairs. 
+            //Dictionary<String, Int32> d = new Dictionary<String, Int32>();
+            //d.Add("Maths", 45);
+            //d.Add("Science", 28);
+            //d.Add("English", 34);
+            //d.Remove("Maths");
+            //Console.WriteLine(d.ContainsKey("Maths"));
+            //Console.WriteLine(d.ContainsValue(34));
+            //d.Clear();
+            //foreach (KeyValuePair<String, int> i in d)
+            //{
+            //    Console.WriteLine("Key is " + i.Key + " Value is " + i.Value);
+            //}
+
+
+            //6. Create a Hashtable collection class object and preform following operations:
+            //a. Add: Adds a key-value pair.
+            //b. Remove: Removes a key-value pair by key.
+            //c. ContainsKey: Checks if a key exists in the hashtable.
+            //d. ContainsValue: Checks if a value exists in the hashtable.
+            //e. Clear: Removes all key-value pairs. 
+
+            Hashtable ht = new Hashtable();
+            ht.Add("Maths", 45);
+            ht.Add("Science", 28);
+            ht.Add("English", 34);
+            ht.Remove("Science");
+            Console.WriteLine(ht.ContainsValue(34));
+            Console.WriteLine(ht.ContainsKey("English"));
+            ht.Clear();
+            foreach (DictionaryEntry i in ht)
+            {
+                Console.WriteLine("Key is: " + i.Key+ " Value is: "+i.Value );
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Asp.net/lab1/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////1. Write a program to print your name, address, contact number & city. 
+            //Console.WriteLine("Nimavat Mansi Dilipbhai");
+            //Console.WriteLine("Sarvad Ta:Maliya Dist:Morbi");
+            //Console.WriteLine("XXXXXXXXXX");
+            //Console.WriteLine("Morbi");
+
+            // //2. Write a program to get two numbers from user and print those two numbers.
+            // Console.WriteLine("Enter 1st number");
+            //int a=Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter 2nd number");
+            // int b = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Your 1st number is:" + a);
+            // Console.WriteLine("Your 2nd number is:"+b);
+
+            ////3.Write program to prompt a user to input his/her name and country name  and then
+            ////output will be shown as given: Hello <yourname> from country <countryname> 
+            //Console.WriteLine("Enter your name");
+            //String a=Console.ReadLine();
+            //Console.WriteLine("Enter your country name");
+            //String b = Console.ReadLine();
+            //Console.WriteLine("Hello "+a+" from country "+b);
+
+            //// 4.Write a program to calculate the size of the area
+            //// in square-feet based on Specified length and width.
+            // Console.WriteLine("Enter length");
+            //int a=Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter width");
+            // int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your area is " + a * b +"sq.ft");
+
+            ////5. Write a program to calculate area of Square, Rectangle and Circle. 
+            //Console.WriteLine("Enter length ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter width ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter radius ");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Area of Square is " + a * a);
+            //Console.WriteLine("Area of Rectangle is " + a * b);
+            //Console.WriteLine("Area of Circle is:" + Math.PI * c * c);
+
+            ////6.Write a program to calculate Celsius to Fahrenheit and vice-versa using function.
+            //Console.WriteLine("Enter Celsius ");
+            //double c=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Fahrenheit is " + Far(c));
+            //Console.WriteLine("Enter Fahrenheit ");
+            //double f = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Fahrenheit is "+Cel(f));
+
+            //7. Write a program to find out Simple Interest using function. (I = PRN/100) 
+            //Console.WriteLine("Enter 1st number ");
+            //int p= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter 2nd number ");
+            //int r = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter 3rd number ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("SI is " + SI(p, r, n));
+
+
+
+            ////8.Write a program to create a Simple Calculator for two numbers 
+            ////(Addition, Multiplication, Subtraction, Division) [Also using if…else & Switch Case] Write a program to create a Simple Calculator for two numbers (Addition, Multiplication, Subtraction, Division)
+            ////[Also using if…else & Switch Case]
+            //Console.WriteLine("Enter 1st number ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter 2nd number ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the operator ");
+            //char c = Convert.ToChar(Console.ReadLine());
+            //if (c == '+')
+            //{
+            //    Console.WriteLine("Addition of " + a + "+" + b + "is " + (a + b));
+            //}
+            //else if(c=='-')
+            //{
+            //    Console.WriteLine("Subtraction of " + a + "-" + b + "is " + (a - b));
+            //}
+            //else if (c == '*')
+            //{
+            //    Console.WriteLine("Multiplication of " + a + "*" + b + "is " + (a * b));
+            //}
+            //else if (c == '/')
+            //{
+            //    Console.WriteLine("Division of " + a + "/" + b + "is " +Convert.ToDouble (Convert.ToDouble(a) / Convert.ToDouble(b)));
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Operator");
+            //}
+
+            ////9.Write a program to Swapping without using third variable. 
+            //Console.WriteLine("Enter 1st number ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter 2nd number ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("old numbers are:" + a + " " + b);
+            //a = a -b ;
+            //b = a + b;
+            //a = b - a;
+
+            //Console.WriteLine("New numbers are;" + a + " " + b);
+
+            ////10. Write a program to find maximum numbers from given 3
+            ////numbers using ternary operator. 
+            //Console.WriteLine("Enter 1st number ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter 2nd number ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter 1st number ");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Maximum is " +( (a > b) ? ((a > c) ? a : c):((b>c) ? b : c)));
+            //char val = Console.Read();
+            //Console.WriteLine(val);
+        }
+        static double Far(double x)
+        {
+            return (x*9/5)+32;
+        }
+        static double Cel(double x)
+        {
+            return (x - 32) * 5 / 9;
+        }
+        static double SI(int x, int y, int z)
+        {
+            return ((Convert.ToDouble(x) * Convert.ToDouble(y) * Convert.ToDouble(z)) / 100.0);
+        }
+    }
+}

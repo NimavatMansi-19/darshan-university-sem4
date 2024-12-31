32cbaa57e8e1509594895756b914db797f3b96b9
Nimavat Mansi <144536348+NimavatMansi-19@users.noreply.github.com>
2024-12-31 20:39:55 +0530

    Add files via upload

---
+++ Asp.net/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult You()
+        {
+            return View();
+        }
+        public IActionResult Me()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.net/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Price()
+        {
+            return View();
+        }
+        public IActionResult Item()
+        {
+            return View();
+        }
+    }
+}
